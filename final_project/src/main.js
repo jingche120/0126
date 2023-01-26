619909d7e190deb5860c1ea74587284e823257fe
jingche120 <77791427+jingche120@users.noreply.github.com>
2023-01-26 22:31:07 +0800

    製作登入頁面

--- final_project/src/main.js
+++ final_project/src/main.js
@@ -1,5 +1,12 @@
 import { createApp } from 'vue';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
 import App from './App.vue';
 import router from './router';
 
-createApp(App).use(router).mount('#app');
+// 因為要加入.use(axuios)，所以要把這行改掉
+// createApp(App).use(router).mount('#app');
+const app = createApp(App);
+app.use(router);
+app.use(VueAxios, axios);
+app.mount('#app');
